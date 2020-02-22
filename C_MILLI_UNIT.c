335fb4187a0986d57e46df664e36be8959a17bef
Dug <guillaume.dugast@gmail.com>
2020-02-22 23:44:01 +0100

    deferred log is now working

--- C_MILLI_UNIT.c
+++ C_MILLI_UNIT.c
@@ -3,7 +3,9 @@
 static char messageBuffer[1024];
 
 
-#define LOG(...)  do{printf("M0@ : "); printf(__VA_ARGS__); }while(0)
+#define PRINT_LOG()  do{ printf ("%s",messageBuffer); }while(0)
+#define LOG(...)  do{ sprintf(messageBuffer,"%s M0@ : ",messageBuffer); sprintf(messageBuffer,"M0@ : "__VA_ARGS__); }while(0)
+
 #define EXPECT_EQ(name,expected,observed) do{(expected==observed)?LOG("%s : SUCCESS\n",name):LOG("%s : FAILURE - expected %d / observed %d\n",name,expected,observed);}while(0)
 #define STEP(X,Y) void X(int a, int b) { LOG(#X" - setup\n"); Y LOG(#X" - teardown\n"); PRINT_LOG();}
 
@@ -18,10 +20,9 @@ int add(int x, int y)
 
 int main()
 {
+
     LOG("test %d\n",1);
+    PRINT_LOG();
     //toto(1,2);
     return 0;
 }
-
-//#define PRINT_LOG()  do{ printf ("%s",messageBuffer); }while(0)
-//#define LOG(...)  do{ sprintf(messageBuffer,"%s M0@ : ",messageBuffer); sprintf(messageBuffer,"%s M0@ : ",messageBuffer)(__VA_ARGS__); }while(0)
