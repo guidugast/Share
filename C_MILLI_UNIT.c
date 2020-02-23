cc9050ec0b4f6adb7999ca90ccbd4ca4e7c51906
Dug <guillaume.dugast@gmail.com>
2020-02-23 02:39:00 +0100

    X macro added for command list + deferred log is now really working

--- C_MILLI_UNIT.c
+++ C_MILLI_UNIT.c
@@ -1,28 +1,64 @@
 #include <stdio.h>
 
-static char messageBuffer[1024];
+
+typedef void (*callback_t)(int a, int b);
+
+typedef struct 
+{
+    char * name;    
+    callback_t callback;
+}command_t;
 
 
-#define PRINT_LOG()  do{ printf ("%s",messageBuffer); }while(0)
-#define LOG(...)  do{ sprintf(messageBuffer,"%s M0@ : ",messageBuffer); sprintf(messageBuffer,"M0@ : "__VA_ARGS__); }while(0)
+#define COMMAND_LIST(X) \
+        X(toto) \
+        X(help) \
+        //end of list
+        
+#define CALLBACK_DEC(COMMAND_NAME)  void _##COMMAND_NAME(int a, int b);
+COMMAND_LIST(CALLBACK_DEC);
+#undef CALLBACK_DEC
 
-#define EXPECT_EQ(name,expected,observed) do{(expected==observed)?LOG("%s : SUCCESS\n",name):LOG("%s : FAILURE - expected %d / observed %d\n",name,expected,observed);}while(0)
-#define STEP(X,Y) void X(int a, int b) { LOG(#X" - setup\n"); Y LOG(#X" - teardown\n"); PRINT_LOG();}
+#define COMMAND_DEF(COMMAND_NAME)  (command_t){ #COMMAND_NAME, & _##COMMAND_NAME },
+static command_t commands[] =
+{
+  COMMAND_LIST(COMMAND_DEF)
+};
+#undef COMMAND_DEF
+
+
+
+#define INIT_LOG(X) static char messageBuffer[1024]
+#define APPEND_LOG(X) do{ char temp_buf[1024]; sprintf(temp_buf, "%s %s",messageBuffer, X); sprintf(messageBuffer,"%s",temp_buf);}while(0)
+#define LOG_LINE(...)  do{APPEND_LOG("M0@ : "); char va_args_buf[256]; sprintf(va_args_buf,__VA_ARGS__); APPEND_LOG(va_args_buf); APPEND_LOG("\n");}while(0)
+#define PRINT_LOG()  do{ printf ("%s\n",messageBuffer); }while(0)
+
+
+#define EXPECT_EQ(name,observed,expected) do{if(expected==observed){LOG_LINE(#name" : SUCCESS");}else{LOG_LINE(#name" : FAILURE - expected %d / observed %d",expected,observed);}}while(0)
+#define STEP(STEP_NAME,CODE) void _##STEP_NAME(int a, int b) { LOG_LINE(#STEP_NAME" - setup"); CODE LOG_LINE(#STEP_NAME" - teardown\n"); }
+
+
+INIT_LOG();
 
 int add(int x, int y)
 {
     return x+y;
 }
 
-/*STEP(toto,
-    EXPECT_EQ("add - nominal",add(1,1),2);
-)*/
+STEP(help,
+    LOG("this is help");
+)
+
+STEP(toto,
+    EXPECT_EQ(add_nominal,add(1,1),2);
+    EXPECT_EQ(add_degraded,add(1,2),2);
+)
 
 int main()
 {
-
-    LOG("test %d\n",1);
+    
+    commands[0].callback(1,2);
+    commands[1].callback(1,2);
     PRINT_LOG();
-    //toto(1,2);
     return 0;
 }
